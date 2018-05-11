a1ec4918a7cbfee16d7bc59797f6ee145e732378
philicevic <ehrenberg.ph@gmail.com>
2018-05-12 00:59:17 +0200

    Update "new clan"-section

--- resources/views/main/dashboard.blade.php
+++ resources/views/main/dashboard.blade.php
@@ -12,44 +12,36 @@
         <div class="row justify-content-center">
             @foreach ($clans as $clan)
                 <div class="col-md-4">
-                <!-- Widget: user widget style 1 -->
-                <div class="card card-widget widget-user">
-                    <!-- Add the bg color to the header using any of the bg-* classes -->
-                    <div class="widget-user-header bg-info-active">
-                        <h3 class="widget-user-username">{{ $clan->name }}</h3>
-                        <h5 class="widget-user-desc">Your Role</h5>
-                    </div>
-                    <a href="http://{{ $clan->subdomain }}{{ config('session.domain') }}">
-                        <div class="widget-user-image">
-                            <img class="img-circle elevation-2" src="/img/adminlte/default-150x150.png" alt="User Avatar">
+                    <!-- Widget: user widget style 1 -->
+                    <div class="card card-widget widget-user">
+                        <!-- Add the bg color to the header using any of the bg-* classes -->
+                        <div class="widget-user-header bg-info-active">
+                            <h3 class="widget-user-username">{{ $clan->name }}</h3>
+                            <h5 class="widget-user-desc">Your Role</h5>
                         </div>
-                    </a>
-                    <div class="card-footer">
-                        <div class="row">
-                            <div class="col-sm-4 border-right">
-                                <div class="description-block">
-                                    <h5 class="description-header">32</h5>
-                                    <span class="description-text">Members</span>
-                                </div>
-                                <!-- /.description-block -->
+                        <a href="http://{{ $clan->subdomain }}{{ config('session.domain') }}">
+                            <div class="widget-user-image">
+                                <img class="img-circle elevation-2" src="/img/adminlte/default-150x150.png" alt="User Avatar">
                             </div>
-                            <!-- /.col -->
-                            <div class="col-sm-4 border-right">
-                                <div class="description-block">
-                                    <h5 class="description-header">3</h5>
-                                    <span class="description-text">Teams</span>
+                        </a>
+                        <div class="card-footer">
+                            <div class="row">
+                                <div class="col-sm-6 border-right">
+                                    <div class="description-block">
+                                        <h5 class="description-header">32</h5>
+                                        <span class="description-text">Members</span>
+                                    </div>
+                                    <!-- /.description-block -->
                                 </div>
-                                <!-- /.description-block -->
-                            </div>
-                            <!-- /.col -->
-                            <div class="col-sm-4">
-                                <div class="description-block">
-                                    <h5 class="description-header">2</h5>
-                                    <span class="description-text">New notifications</span>
+                                <!-- /.col -->
+                                <div class="col-sm-6 border-right">
+                                    <div class="description-block">
+                                        <h5 class="description-header">3</h5>
+                                        <span class="description-text">Teams</span>
+                                    </div>
+                                    <!-- /.description-block -->
                                 </div>
-                                <!-- /.description-block -->
-                            </div>
-                            <!-- /.col -->
+                                <!-- /.col -->
                             </div>
                             <!-- /.row -->
                         </div>
@@ -58,15 +50,36 @@
                 </div>
             @endforeach
             <div class="col-md-4">
-                <div class="card">
-                    <div class="card-header">
-                        <h5 class="m-0">New clan</h5>
+                <div class="card card-widget widget-user widget-add-clan">
+                    <div class="widget-user-header bg-info-active">
+                        <h3 class="widget-user-username">New clan</h3>
+                        <h5 class="widget-user-desc">Find or create a new clan</h5>
                     </div>
-                    <div class="card-body">
+                    {{-- <div class="card-body">
                         <h6 class="card-title">Get started</h6>
 
                         <p class="card-text">with your own clan on cfox.io</p>
                         <a href="{{ route('main.clans.create') }}" class="btn btn-success">Create it now!</a>
+                    </div> --}}
+                    <div class="card-footer">
+                        <div class="row">
+                            <div class="col-sm-6 border-right">
+                                <a href="#">
+                                    <div class="description-block">
+                                        <h5 class="description-header">Find</h5>
+                                        <span class="description-text">an existing clan</span>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="col-sm-6">
+                                <a href="{{ route('main.clans.create') }}">
+                                    <div class="description-block">
+                                        <h5 class="description-header">Create</h5>
+                                        <span class="description-text">a new clan</span>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
