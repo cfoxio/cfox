260be0cbd06bb8f73063e8cf06470a65ad3b9e41
philicevic <ehrenberg.ph@gmail.com>
2018-06-25 00:48:28 +0200

    Change Route Key Name of Team Model to slug

--- app/Team.php
+++ app/Team.php
@@ -3,16 +3,43 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use MartinBean\Database\Eloquent\Sluggable;
 use App\Clan;
 use App\User;
 
 class Team extends Model
 {
-    public function user() {
+    use Sluggable;
+
+    /**
+     * change the routeKeyName for Route Model Binding
+     *
+     * @return string
+     */
+    public function getRouteKeyName() {
+        return 'slug';
+    }
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['created_at', 'updated_at'];
+
+    public function users() {
         return $this->belongsToMany(User::class);
     }
 
     public function clan() {
         return $this->belongsTo(Clan::class);
+    }
+
+    public function teamleader() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function manager() {
+        return $this->belongsTo(User::class);
     }
 }
