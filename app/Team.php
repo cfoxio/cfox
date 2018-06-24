18c59adcfa94e587c183928af3eccfbc53703904
philicevic <ehrenberg.ph@gmail.com>
2018-06-25 01:28:04 +0200

    Update slug config for team model

--- app/Team.php
+++ app/Team.php
@@ -3,13 +3,27 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use MartinBean\Database\Eloquent\Sluggable;
+use Cviebrock\EloquentSluggable\Sluggable;
 use App\Clan;
 use App\User;
 
 class Team extends Model
 {
     use Sluggable;
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
 
     /**
      * change the routeKeyName for Route Model Binding
